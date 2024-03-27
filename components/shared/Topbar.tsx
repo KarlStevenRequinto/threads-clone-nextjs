05d211680384c23fde362cce31e1e1ecad379a40
KarlStevenRequinto <lerdrobott@gmail.com>
2024-03-28 06:43:05 +0800

    Adding assets and replacing image of favicon

--- components/shared/Topbar.tsx
+++ components/shared/Topbar.tsx
@@ -1,7 +1,16 @@
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 const Topbar = () => {
-    return <div>Topbar</div>;
+    return (
+        <nav className="topbar">
+            <Link href="/" className="flex items-center gap-4">
+                <Image src="assets/logo.svg" alt="logo" width={28} height={28} />
+                <p className="text-heading3-bold text-light-1 max-xs:hidden">Threads</p>
+            </Link>
+        </nav>
+    );
 };
 
 export default Topbar;
