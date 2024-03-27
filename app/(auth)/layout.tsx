d318a51037fd9ce203a30f70707cd41ebc6e8391
KarlStevenRequinto <lerdrobott@gmail.com>
2024-03-27 23:50:32 +0800

    authentication

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,20 @@
+import { ClerkProvider } from "@clerk/nextjs";
+import { Inter } from "next/font/google";
+import "../global.css"
+
+export const metadata = {
+    title: "Threads Clone",
+    description: "A Next.js Meta Threads Clone Application",
+};
+
+const inter = Inter({ subsets: ["latin"] });
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+    return (
+        <ClerkProvider>
+            <html lang="en">
+                <body className={`${inter.className} bg-dark-1`}>{children}</body>
+            </html>
+        </ClerkProvider>
+    );
+}
--- app/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-    return (
-        <main>
-            <h1>THREADS</h1>
-        </main>
-    );
-}
