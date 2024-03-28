a8911f0e3968c9b84697c22423ae51105e4b1bbb
KarlStevenRequinto <lerdrobott@gmail.com>
2024-03-29 06:27:26 +0800

    account profile component

---
+++ components/forms/AccountProfile.tsx
@@ -0,0 +1,18 @@
+"use client";
+import React from "react";
+interface Props {
+    user: {
+        id: string;
+        objectId: string;
+        username: string;
+        name: string;
+        bio: string;
+        image: string;
+    };
+    btnTitle: string;
+}
+const AccountProfile = ({ user, btnTitle }: Props) => {
+    return <div>AccountProfile</div>;
+};
+
+export default AccountProfile;
