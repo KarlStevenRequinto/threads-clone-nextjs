8bb3d33bb7ffa21d1b3d7f2f9f6407dca20d84d3
KarlStevenRequinto <lerdrobott@gmail.com>
2024-04-02 02:50:02 +0800

    adding threads, comments ui

--- lib/actions/thread.actions.ts
+++ lib/actions/thread.actions.ts
@@ -25,3 +25,33 @@ export async function createThread({ text, author, communityId, path }: Params) 
         throw new Error(`Error creating thread: ${error.message}`);
     }
 }
+
+export async function fetchPosts(pageNumber = 1, pageSize = 20) {
+    connectToDB();
+
+    const skipAmount = (pageNumber - 1) * pageSize;
+
+    const postsQuery = Thread.find({ parentId: { $in: [null, undefined] } })
+        .sort({ createdAt: "desc" })
+        .skip(skipAmount)
+        .limit(pageSize)
+        .populate({ path: "author", model: User })
+        .populate({
+            path: "children",
+            populate: {
+                path: "author",
+                model: User,
+                select:"_id name parentId image"
+            },
+        });
+
+    const totalPostsCount = await Thread.countDocuments({
+        parentId: { $in: [null, undefined] },
+    });
+
+    const posts = await postsQuery.exec();
+
+    const isNext = totalPostsCount > skipAmount + posts.length;
+
+    return { posts, isNext };
+}
--- next.config.js
+++ next.config.js
@@ -26,6 +26,7 @@ const nextConfig = {
       typescript: {
         ignoreBuildErrors: true,
       },
+      domains: ['utfs.io'],
     },
   };
   
